abd10dd081aa9aca380cf62337863b6b8782fdea
Shiyan Shirani <shiyan99s@gmail.com>
2021-06-17 13:33:23 +0530

    templates added

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'Shiyan Shirani',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': 'June 16, 2021'
+    },
+    {
+        'author': 'Anandita Puria',
+        'title': 'Blog Post 2',
+        'content': 'First Post Content',
+        'date_posted': 'June 17, 2021'
+    }
+]
+
+
 def home(request):
-    return render(request, 'blog/home.html ')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html') 
+    return render(request, 'blog/about.html', {'title' : 'About'}) 
